a9102f2c9c722446fb0b5a01fd8a83363c57dae7
merouane <ikkisauto@gmail.com>
2024-06-18 15:52:04 +0100

    update home page frontend

--- lfrajastore/app/views.py
+++ lfrajastore/app/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Product
 
 
 def product_list(request):
-    products = Product.objects.all().values(
-        'id', 'name', 'description', 'price', 'file')
-    products_list = list(products)  # Convert the QuerySet to a list
-    return JsonResponse(products_list, safe=False)
+    products = Product.objects.all()
+    data = [{'id': product.id, 'name': product.name, 'price': product.price,
+             'file': product.file.url} for product in products]
+    return JsonResponse(data, safe=False)
